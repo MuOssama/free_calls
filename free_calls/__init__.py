4147d74f7b229d208a223acac7e111136e373c26
Mustapha Ossama <109214165+MuOssama@users.noreply.github.com>
2025-12-03 18:40:50 +0200

    Delete free_calls directory

--- free_calls/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-free_calls - Make API requests using free-tier quota across multiple keys automatically
-"""
-
-from .manager import FreeCallsManager
-
-__version__ = "0.1.0"
-__all__ = ["FreeCallsManager"]
--- free_calls/exceptions.py
+++
@@ -1,6 +0,0 @@
-"""Custom exceptions for free_calls"""
-
-
-class NoValidAPIKeyError(Exception):
-    """Raised when all API keys have been exhausted"""
-    pass
